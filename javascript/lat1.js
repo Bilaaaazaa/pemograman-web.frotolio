4222e376ff1ca4d6e5ecf8ef90c00dda5a3b31b4
bilaaaazaa <141792912+Bilaaaazaa@users.noreply.github.com>
2023-10-02 14:50:46 +0700

    Add files via upload

---
+++ javascript/lat1.js
@@ -0,0 +1,14 @@
+document.getElementById("text").innerHTML = "MY BIODATA"
+
+let nama = "Nama : Bela Nurseha";
+document.getElementById("nama").innerHTML = nama;
+
+let alamat = "Alamat : Kp.Nagrog RT.03 RT.07";
+document.getElementById("alamat").innerHTML = alamat;
+
+let ttg = "Tanggal Lahir : 13 Januari 2007";
+document.getElementById("ttg").innerHTML = ttg;
+
+let hoby = "Hoby : Rebahan,Dengerin Musik";
+document.getElementById("hoby").innerHTML = hoby;
+
