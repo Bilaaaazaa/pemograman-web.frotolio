d6354656b046881c7faf90ceb79a4e1700cadbd9
bilaaaazaa <141792912+Bilaaaazaa@users.noreply.github.com>
2023-12-04 13:32:26 +0700

    Add files via upload

---
+++ bubel xi pplg 1/swicth/biodata/biodata.js
@@ -0,0 +1,11 @@
+let nama="Bela Nurseha";
+document.getElementById("nama").innerHTML = nama
+
+let alamat = "Kp.Nagrog";
+document.getElementById("alamat").innerHTML = alamat
+
+let ttg = "Tasikmalaya, 13 januari 2007";
+document.getElementById("ttg").innerHTML = ttg
+
+let hoby = "Ngamer,cosplay musik";
+document.getElementById("hoby").innerHTML = hoby
